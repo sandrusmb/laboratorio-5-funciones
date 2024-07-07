abe0d1ab7f3c0252c58fa2318d006d2f580de02e
sandrusmb <sandrusmb@gmail.com>
2024-07-07 15:14:44 +0200

    Redo logic in a way I understand

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,56 @@
-const previousButton = document.querySelector(
+let numeroTurno = document.querySelector(".numero-turno");
+const botonAnterior = document.querySelector(
   ".previous-button"
 ) as HTMLButtonElement;
-const nextButton = document.querySelector(".next-button") as HTMLButtonElement;
-const resetButton = document.querySelector(
+const botonSiguiente = document.querySelector(
+  ".next-button"
+) as HTMLButtonElement;
+const botonBorrar = document.querySelector(
   ".reset-button"
 ) as HTMLButtonElement;
-const turnoDisplay = document.querySelector(".numero-turno") as HTMLElement;
-const manualInput = document.querySelector(".input") as HTMLInputElement;
-const submitButton = document.querySelector(
+const inputParaEscribir = document.querySelector(".input") as HTMLInputElement;
+const botonEnviar = document.querySelector(
   ".submit-button"
 ) as HTMLButtonElement;
 
-function forwardCounter(): void {
-  if (turnoDisplay !== null) {
-    const paintedNumber: string = turnoDisplay.innerHTML;
-    const numberPaintedNumber: number = parseInt(paintedNumber);
-    const resultado = numberPaintedNumber - 1;
-    turnoDisplay.innerHTML = (resultado >= 0 ? resultado : 0)
-      .toString()
-      .padStart(2, "0");
-  }
-}
+botonAnterior?.addEventListener("click", restarTurno);
 
-function backwardCounter(): void {
-  if (turnoDisplay !== null) {
-    const paintedNumber: string = turnoDisplay.innerHTML;
-    const numberPaintedNumber: number = parseInt(paintedNumber);
-    const resultado = numberPaintedNumber + 1;
-    turnoDisplay.innerHTML = resultado.toString().padStart(2, "0");
-  }
-}
-
-function resetCounter(): void {
-  if (turnoDisplay !== null) {
-    const resultado = 0;
-    turnoDisplay.innerHTML = resultado.toString().padStart(2, "0");
-    manualInput.value = "0";
-  }
-}
-
-function manualCounter(): void {
-  if (turnoDisplay !== null) {
-    const inputNumber: number = parseInt(manualInput.value);
-    if (!isNaN(inputNumber)) {
-      turnoDisplay.innerHTML = inputNumber.toString().padStart(2, "0");
+function restarTurno(): void {
+  if (numeroTurno !== null) {
+    let numeroTurnoInterior: string = numeroTurno.innerHTML;
+    let numeroTurnoInteriorParseado: number = parseInt(numeroTurnoInterior);
+    let resultado: number = numeroTurnoInteriorParseado - 1;
+    numeroTurno.innerHTML = resultado.toString().padStart(2, "0");
+    if (resultado < 0) {
+      borrarTurno();
     }
   }
 }
 
-previousButton?.addEventListener("click", forwardCounter);
-nextButton?.addEventListener("click", backwardCounter);
-resetButton?.addEventListener("click", resetCounter);
-submitButton?.addEventListener("click", manualCounter);
+botonSiguiente?.addEventListener("click", sumarTurno);
+
+function sumarTurno(): void {
+  if (numeroTurno !== null) {
+    let numeroTurnoInterior: string = numeroTurno.innerHTML;
+    let numeroTurnoInteriorParseado: number = parseInt(numeroTurnoInterior);
+    let resultado: number = numeroTurnoInteriorParseado + 1;
+    numeroTurno.innerHTML = resultado.toString().padStart(2, "0");
+  }
+}
+
+botonBorrar?.addEventListener("click", borrarTurno);
+
+function borrarTurno(): void {
+  if (numeroTurno !== null) {
+    numeroTurno.innerHTML = "00";
+  }
+}
+
+botonEnviar?.addEventListener("click", escribirTurno);
+
+function escribirTurno(): void {
+  const inputParaEscribirValue: string = inputParaEscribir.value;
+  if (numeroTurno !== null) {
+    numeroTurno.innerHTML = inputParaEscribirValue.padStart(2, "0");
+  }
+}
